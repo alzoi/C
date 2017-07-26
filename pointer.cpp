18467e7c5e5102b4741d6d828797477722a3faa8
alzoi <alzoi@ya.ru>
2017-07-26 21:59:43 +0500

    Create pointer.cpp

---
+++ pointer.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+
+#include <stdio.h>
+#include <stdlib.h> 
+
+void foo1(int *p){
+// Указатель передаётся по значению (копия указателя создаётся на стеке).
+    p = (int*)malloc(80);
+    printf("%p\n", p);
+    *p = 101;
+}
+
+void foo2(int **p){
+// Указатель на указатель передаётся по значению (копия указателя на указатель создаётся на стеке).
+    // Разыменовав указатель на указатель, получим значение = адресу переданного элемента, а в эту функцию передаётся указатель на int. 
+    *p = (int*)malloc(80);
+    printf("%p\n", *p);
+    **p=101;
+}
+
+void test1(void){
+    int p=7;
+    int *k = &p;
+    printf("%p\n", k);
+    foo1(k);
+    printf("%p\n", k);
+    printf("%d\n", *k);
+}
+
+void test2(void){
+    int p=7;
+    int *k = &p;
+    printf("%p\n", k);
+    foo2(&k);
+    printf("%p\n", k);
+    printf("%d\n", *k);
+}
+
+int main()
+{
+    test1();
+    printf("\n");
+    test2();
+    
+}
