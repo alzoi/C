2bbf61d8ba7bad957b8b26e15273ae715e34ae4f
alzoi <alzoi@ya.ru>
2017-07-26 22:02:13 +0500

    Update pointer.cpp

--- pointer.cpp
+++ pointer.cpp
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
-void foo1(int *p){
+void foo1(int *p) {
 // Указатель передаётся по значению (копия указателя создаётся на стеке) после выхода из функции указатель теряется.
     p = (int*)malloc(80);
     printf("%p\n", p);
     *p = 101;
 }
 
-void foo2(int **p){
+void foo2(int **p) {
 // Указатель на указатель передаётся по значению (копия указателя на указатель создаётся на стеке).
     // Разыменовав указатель на указатель, получим значение = адресу переданного элемента, а в эту функцию передаётся указатель на int.    
     *p = (int*)malloc(80);
@@ -19,7 +19,7 @@ void foo2(int **p){
     **p=101;
 }
 
-void test1(void){
+void test1(void) {
     int p=7;
     int *k = &p;
     printf("%p\n", k);
@@ -28,7 +28,7 @@ void test1(void){
     printf("%d\n", *k);
 }
 
-void test2(void){
+void test2(void) {
     int p=7;
     int *k = &p;
     printf("%p\n", k);
@@ -37,12 +37,11 @@ void test2(void){
     printf("%d\n", *k);
 }
 
-int main()
-{
+int main() {
     test1();
     printf("\n");
     test2();
-    
+    return 0;
 }
 
 /*
